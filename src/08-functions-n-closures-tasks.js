016287096a722a84ede93c314e961b9573c1a4f6
frontending <mymage259@gmail.com>
2022-07-25 20:01:56 +0300

    fix: restore backup

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -42,10 +42,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(exponent) {
-  return function (num) {
-    return num ** exponent;
-  };
+function getPowerFunction(/* exponent */) {
+  throw new Error('Not implemented');
 }
 
 /**
